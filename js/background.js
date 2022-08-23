f64dd2ff153e9dc7c9b0d3b9a58a42c5ece7e09f
5onze <darkerhorn@gmail.com>
2022-08-24 06:43:03 +0900

    update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
-const images = ['0.jpeg', '1.jpeg', '2.jpeg'];
+const images = ['0.jpg', '1.jpg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+const bgc = document.querySelector('.background');
 const bgImage = document.createElement('img');
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.alt = 'background images';
 
-document.body.appendChild(bgImage);
+bgc.append(bgImage);
