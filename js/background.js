7bd37683e8f1a1af4105e521bfe204fffd4e7ea5
5onze <darkerhorn@gmail.com>
2022-08-29 04:37:03 +0900

    update background

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
-const images = ['0.jpg', '1.jpg'];
+const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgc = document.querySelector('.background');
-const bgImage = document.createElement('img');
+// const bgImage = document.createElement('img');
+// bgImage.src = `img/${chosenImage}`;
+// bgImage.alt = 'background images';
+//bgc.append(bgImage);
 
-bgImage.src = `img/${chosenImage}`;
-bgImage.alt = 'background images';
-
-bgc.append(bgImage);
+bgc.style.backgroundImage = `url(img/${chosenImage})`;
