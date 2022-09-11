0191cf53bb8f618f3fc68134ae08860d94a6ddf0
5onze <darkerhorn@gmail.com>
2022-09-12 04:28:35 +0900

    update url

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = '4506ef4e290d6eda0264e94930e5bd60';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul,korea&appid=${API_KEY}&lang=kr&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
