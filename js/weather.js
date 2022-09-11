0d7831a42ef26661f58e18038580dd8e7cb4e13d
5onze <darkerhorn@gmail.com>
2022-09-12 03:58:43 +0900

    update weather

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,19 @@ const API_KEY = '4506ef4e290d6eda0264e94930e5bd60';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector('#weather span:first-child');
-      const city = document.querySelector('#weather span:last-child');
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      const tempIcon = document.querySelector('#weather .temp-icon');
+      const temp = document.querySelector('#weather .temp');
+      const city = document.querySelector('#weather .city');
+      const icon = data.weather[0].icon;
+      const img = document.createElement('img');
+      city.innerText = `${data.name}-si`;
+      temp.innerText = `${Math.floor(data.main.temp)}°`;
+      tempIcon.prepend(img);
+      img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
     });
 }
 function onGeoError() {
