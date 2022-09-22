b4f7dd0b19fcee5a67e41f52363970d8e7329cde
5onze <darkerhorn@gmail.com>
2022-09-23 00:36:42 +0900

    update today text

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,16 @@
 const clock = document.querySelector('h2#clock');
+const today = document.querySelector('#clock-date');
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}`;
+
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  const month = date.getMonth();
+  const days = date.getDate();
+  today.innerText = `${month + 1}월 ${days}일 ${week[date.getDay()]}요일`;
 }
 getClock();
 setInterval(getClock, 1000);
